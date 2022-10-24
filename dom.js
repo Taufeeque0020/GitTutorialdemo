ccf53a5390f1a1f0d91feacac7a61c76690c3374
Taufeeque0020 <116538249+Taufeeque0020@users.noreply.github.com>
2022-10-24 15:12:38 +0530

    js

---
+++ dom.js
@@ -0,0 +1,31 @@
+// EXAMINE THE DOCUMENT OBJECT //
+
+//console.dir(document);
+////console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title = 123;
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+// document.all[10].textXontent = "Hello";
+//console.log(document.forms);
+//console.log(document.links;
+//console.log(documnet.images);
+
+// GETELEMENTBYID //
+//console.log(document.getElementById("header-Title"));
+//var headerTitle = document.getElementById("header-Title");
+//var header = document.getElementById("main-header");
+console.log(headerTitle);
+//headerTitle.textContent = "Hello";
+//headerTitle.innerText = "Goodbye";
+//console.log(headerTitle.innerText);
+//headerTitle.innerHTML = "<h3>Hello</h3>";
+//header.style.borderBottom = "solid 3px #000"
+
+// GETELEMENTBYCLASSNAME //
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
